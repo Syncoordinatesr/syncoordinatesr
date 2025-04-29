8f0d3c11df74dc4a63e275d011745661153c83aa
luizfernando103 <79534453+luizfernando103@users.noreply.github.com>
2025-04-29 11:50:58 -0300

    Updated syncoordinates function
    - Refactored code
    - Refactored documentation

--- R/synthetic_coordinates_function.R
+++ R/synthetic_coordinates_function.R
@@ -8,8 +8,11 @@
 #'
 #' @param  dataset  A data frame with all the information except the coordinates
 #' @param  coord  An object with two columns indicating the longitude and latitude respectively of the elements in the dataset
+#' @param  limits  Object that is a vector of the dimensions where the grid cells will be created, specified by the sequence xmin, xmax, ymin, ymax. The default is to use the maximum and minimum of the coords object.
 #' @param  grid  The grid represents the quantities of divisions that will be made in the location. Bigger the grid, closer the synthetic coordinates are to the real coordinates. With a default result of (grid = 10)
 #' @param  continuous  An object that indicates which columns in the dataset correspond to continuous variables. The default is FALSE which means that there is none continuous variable. (Still not adapted for cases with more than one continuous variable)
+#' @param  restricted_area  Logical argument indicating whether there are restricted areas where synthetic geographic coordinates should not be generated. The default is FALSE, meaning there are no restricted areas.
+#' @param  coord_restricted_area  Object with two columns indicating the longitude and latitude of the points that form the restricted areas. By default, for the object to be considered a polygon, the first longitude and latitude points must be identical to the last longitude and latitude points.
 #' @param  list_mcmc  Output of the mcmc function
 #' @param  n.syn  Number of synthetic database that will be returned
 #'
@@ -20,30 +23,37 @@
 #' Disponível em: \url:{http//est.ufmg.br/portal/arquivos/mestrado/dissertacoes/dissertacao_Leticia_Silva_Nunes.pdf}. Acesso em: 2 mar. 2022.
 #'
 #' @examples
-#'   syncoordinates(dataset = my_database, lambda = lambda_from_mcmc, n.syn = 3)
-#'
+#'    syncoordinates(dataset = my_database , coord = my_coords, grid = 10, continuous = FALSE, restricted_area = FALSE, list_mcmc = my_mcmc, n.syn = 5)
 #' @export
 
-syncoordinates <- function(dataset, coord, limits = c(), grid = 10, 
+syncoordinates <- function(dataset, coord, limits = c(), grid = 10,
                            continuous = FALSE,
                            restricted_area = FALSE,
                            coord_restricted_area,
                            list_mcmc, n.syn = 5){
-  
-  
+
+
   saida = prepare_data(dataset, coord, limits, grid, continuous)
   mapply(assign, names(saida), saida, MoreArgs=list(envir = globalenv()))
   mapply(assign, names(list_mcmc), list_mcmc, MoreArgs=list(envir = globalenv()))
-  
-  
+
+
   int.syn = floor((S-burn-1)/(n.syn-1))
+
   list.syn = seq(burn+1, burn+(n.syn-1)*int.syn+1, by=int.syn)
-  
+
+
+  cat("Dimensões de lambda:\n")
+  print(dim(lambda))
+  cat("Lista de list.syn:\n")
+  print(list.syn)
+
   m = length(list.syn)
   prob = array(0, dim=c(G,m,B))
-  
+
+  if(restricted_area){
   coord_restricted_area = as.matrix(coord_restricted_area)
-  
+
   initial_point = FALSE
   polygons = c()
   for (i in 1:dim(coord_restricted_area)[1]) {
@@ -61,12 +71,13 @@ syncoordinates <- function(dataset, coord, limits = c(), grid = 10,
       sub_polygons = c(sub_polygons, c(coord_restricted_area[i,1], coord_restricted_area[i,2]))
     }
   }
-  
+
   n_polygons = length(polygons)
   cat("Warning: Number of polygons informed in the restricted area = ",
       n_polygons, "\n")
   restricted_area_sf = st_multipolygon(list(polygons))
-  
+  }
+
   if(restricted_area != FALSE){
     lon_vector = lonvec
     lat_vector = latvec
@@ -75,7 +86,7 @@ syncoordinates <- function(dataset, coord, limits = c(), grid = 10,
     size_lat_vector = length(lat_vector) - 1
     intersect_list = c()
     grid_list = c()
-    
+
     for(j in 1:size_lat_vector){
       for(i in 1:size_lon_vector){
         coords = rbind(c(lon_vector[i], lat_vector[j]),
@@ -106,7 +117,7 @@ syncoordinates <- function(dataset, coord, limits = c(), grid = 10,
   }else{
     a_vec = rep(1, (grid*grid))
   }
-  
+
   for(k in 1:m){
     for(j in 1:B){
       sum_lambda = sum(lambda[, list.syn[k], j])
@@ -115,61 +126,68 @@ syncoordinates <- function(dataset, coord, limits = c(), grid = 10,
       }
     }
   }
-  
-  
+
+
   loc = matrix(0, n, m)
-  
+
   for(k in 1:m){
     for (j in 1:B){
       aux = comb==j
       loc[aux,k] = sample(c(1:G), sum(aux), prob=prob[,k,j], rep=T)
     }
   }
-  
+
   coord.syn = array(0, c(n,2,m))
   aux1 = array(0, c(n,2,m))
-  
+
   for(k in 1:m){
     aux1[,,k] = vec2mat(loc[,k], grid)
-    
+
     for (i in 1:n){
       flag = TRUE
       while(flag){
         longitude_candidate = runif(1, lonvec[aux1[i,1,k]], lonvec[aux1[i,1,k]+1])
         latitude_candidate = runif(1, latvec[aux1[i,2,k]], latvec[aux1[i,2,k]+1])
-        
+
         ponto = st_point(c(longitude_candidate, latitude_candidate))
-        arr = array(0, length(restricted_area_sf[[1]]))
-        
-        idx = 1
-        for(ra in restricted_area_sf[[1]]){
-          ra = st_polygon(list(ra))
-          resultado = st_intersects(ponto, ra)
-          arr[idx] = !is.na(resultado[1] == 1)
-          idx = idx + 1
+
+        if(restricted_area){
+          arr = array(0, length(restricted_area_sf[[1]]))
+
+          idx = 1
+
+          for(ra in restricted_area_sf[[1]]){
+              ra = st_polygon(list(ra))
+              resultado = st_intersects(ponto, ra)
+              arr[idx] = !is.na(resultado[1] == 1)
+              idx = idx + 1
+          }
+
+          flag = sum(arr)
+        }else{
+          flag = FALSE
         }
-        flag = sum(arr)
       }
       coord.syn[i,1,k] = longitude_candidate
       coord.syn[i,2,k] = latitude_candidate
     }
   }
-  
-  
+
+
   if(is.numeric(continuous)){
-    
+
     sigma.z = matrix(0, B, vZ)
     mean.z = matrix(0, B, vZ)
-    
+
     for(i in 1:B){
       for(j in 1:vZ){
         sigma.z[i,j] = sd(z.bar[, i, j], na.rm = T)
         mean.z[i,j] = mean(z.bar[, i, j], na.rm = T)
       }
     }
-    
+
     z.syn = array(0, c(n, vZ, m))
-    
+
     for(k in 1:m){
       for(j in 1:vZ){
         for(i in 1:n){
@@ -183,11 +201,11 @@ syncoordinates <- function(dataset, coord, limits = c(), grid = 10,
         }
       }
     }
-    
+
   }
-  
+
   if(is.numeric(continuous)){
-    return(list(coord.syn, z.syn))
+    return(list(coord.syn = coord.syn, z.syn =  z.syn))
   } else{
     return(list(coord.syn))
   }
